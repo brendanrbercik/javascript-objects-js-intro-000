787d51a35613a0555773ce7aa92b31a2ad9e0b24
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-21 21:15:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {['The Beatles']: 'Hey Jude', ['Led Zeppelin']: 'Kashmir'};
+var playlist = {};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, {artistName : songTitle});
