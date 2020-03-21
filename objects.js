fa4de720071b15aac29b747cc92e670b0b4a73e9
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-21 21:17:46 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
-var playlist = {};
+var playlist = { Band1 : 'Song 1', Band2: 'Song 2'};
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({}, playlist, {artistName : songTitle});
-  return playlist;
-}
+// function updatePlaylist(playlist, artistName, songTitle) {
+//   Object.assign({}, playlist, {artistName : songTitle});
+//   return playlist;
+// }
